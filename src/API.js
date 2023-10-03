37ac366372d59a2cb1c965f1725a253d8c969d3e
Nonso Mgbechi <mgbechinonso@gmail.com>
2023-10-03 23:06:47 +0200

    code formatting & renamed GeoField to GeoFeed

--- src/API.js
+++ src/API.js
@@ -1,34 +1,33 @@
-const MapStation = require('./Entity/MapStation');
-const CityFeed= require('./Entity/CityFeed');
-const Search= require('./Entity/Search');
-const IPFeed= require('./Entity/IPFeed');
-const GeoFeed= require('./Entity/GeoField');
-
+const MapStation = require("./Entity/MapStation");
+const CityFeed = require("./Entity/CityFeed");
+const Search = require("./Entity/Search");
+const IPFeed = require("./Entity/IPFeed");
+const GeoFeed = require("./Entity/GeoFeed");
 
 class API {
-    constructor(apiKey) {
-        this.apiKey = apiKey;
-    }
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
 
-    cityFeed() {
-        return new CityFeed(this.apiKey);
-    }
+  cityFeed() {
+    return new CityFeed(this.apiKey);
+  }
 
-    search() {
-        return new Search(this.apiKey);
-    }
+  search() {
+    return new Search(this.apiKey);
+  }
 
-    geoFeed() {
-        return new GeoFeed(this.apiKey);
-    }
+  geoFeed() {
+    return new GeoFeed(this.apiKey);
+  }
 
-    ipFeed() {
-        return new IPFeed(this.apiKey);
-    }
+  ipFeed() {
+    return new IPFeed(this.apiKey);
+  }
 
-    mapStation() {
-        return new MapStation(this.apiKey);
-    }
+  mapStation() {
+    return new MapStation(this.apiKey);
+  }
 }
 
 module.exports = API;
